521c9952332f862b374c9a1a0180f6852c88ba98
Sonali <maitysonali841@gmail.com>
2025-02-13 22:32:01 +0530

    Build the Copy, Download and Delete icons

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -1,33 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import { Copy, Download, Trash } from "lucide-react";
+import useFetch from "@/hooks/useFetch";
+import { deleteUrl } from "@/db/apiUrls";
+import { BeatLoader } from "react-spinners";
 
 // Fetch Urls from API
-const LinkCard = ({url, fetchUrls}) => {
+const LinkCard = ({ url, fetchUrls }) => {
+    const downloadImage =() => {
+        const imageUrl =url?.qr;
+        const fileName = url?.title;
+        
+        // create anchor tag using document.createElement
+        const anchor =document.createElement("a");
+        anchor.href = imageUrl;                     // set the href attribute and assign imageUrl
+        anchor.download = fileName;                 // set the download attribute and assign fileName
+
+        // append the anchor to the body of the document
+        document.body.appendChild(anchor);         
+
+        // Trigger the download by simulating a click event
+        anchor.click(); 
+
+        // remove the anchor from the body of the document
+        document.body.removeChild(anchor);            
+    };
+
+    // fetch delete url from API
+    const {loading: loadingDelete, fn: fnDelete} = useFetch(deleteUrl, url?.id);
+
+
   return (
     <div className="flex flex-col md:flex-row gap-5 border p-4 bg-gray-900 rounded-lg">
-
-     {/* fetch the qr code from  the column qr in the url table   */}
-         <img src={url?.qr} 
-         className="h-32 object-contain ring ring-blue-500 self-start"
-         alt="qr code" 
-        />  
-
-        {/* Individual person have a  url page */}
-        <Link to={`/link/${url?.id}`} className="flex flex-col flex-1 text-white">
+      {/* fetch the qr code from  the column qr in the url table   */}
+      <img
+        src={url?.qr}
+        className="h-32 object-contain ring ring-blue-500 self-start"
+        alt="qr code"
+      />
+      {/* Individual person have a  url page */}
+      <Link to={`/link/${url?.id}`} className="flex flex-col flex-1 text-white">
         <span className="text-2xl font-extrabold hover:underline cursor-pointer">
-            {url?.title}</span>
+          {url?.title}
+        </span>
+
         <span className="text-xl text-blue-400 font-bold hover:underline cursor-pointer">
-            https://trimrr.in/{url?.custom_url ? url?.custom_url : url.short_url}           {/* If have custom url then show custom url else short url*/}
+          https://trimmer.in/{url?.custom_url ? url?.custom_url : url.short_url}{" "}
+          {/* If have custom url then show custom url else short url*/}
         </span>
+
         <span className="flex items-center gap-1 hover:underline cursor-pointer">
-            {url?.original_url}                                                       {/* To show original url*/}
+          {url?.original_url} {/* To show original url*/}
         </span>
+
         <span className="flex items-end font-extralight text-sm flex-1">
-            {new Date(url?.created_at).toLocaleString()}                        {/* To show created time and date*/}
+          {new Date(url?.created_at).toLocaleString()}{" "}
+          {/* To show created time and date*/}
         </span>
-        </Link>                                   
+      </Link>
+      
+      <div className="flex gap-2">
+        {/* use Navigator function to navigate  and write text to the clipboard and copy this to the edit page(browser) */}
+        <Button onClick={()=> 
+            navigator.clipboard.writeText(`https://trimmer.in/${url?.short_url}`)
+        }>
+            <Copy/>
+        </Button>
+        <Button onClick={()=>downloadImage}>
+            <Download/>
+        </Button>
+        <Button onClick={()=>fnDelete().then(()=> fetchUrls())}>             {/* After Delete then call the fetchUrls again*/} 
+
+            {loadingDelete?<BeatLoader size={5} color="white" />:<Trash/>}   {/* If loading then show loader else show trash icon*/}
+            
+        </Button>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LinkCard;
--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,15 +1,28 @@
 import supabase, {supabaseUrl} from "./supabase";
 
 export async function getUrls(user_id) {
-    let {data, error} = await supabase
-      .from("urls")
-      .select("*")
-      .eq("user_id", user_id);
-  
-    if (error) {
-      console.error(error);
-      throw new Error("Unable to load URLs");
-    }
-  
-    return data;
+  let {data, error} = await supabase
+    .from("urls")
+    .select("*")
+    .eq("user_id", user_id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to load URLs");
   }
+
+  return data;
+}
+
+ //assign the function to the deleteUrls variable
+ export async function deleteUrl(id) {
+  const {data, error} = await supabase.from("urls").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to delete Url");
+  }
+
+  return data;
+}
+
